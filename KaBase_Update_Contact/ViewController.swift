53ac037f049005577e15088fc8ea9054624c9d2c
Viet Asc <vtahdn@gmail.com>
2019-01-08 14:02:26 +0700

    Initial Commit

---
+++ KaBase_Update_Contact/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  KaBase_Update_Contact
+//
+//  Created by Viet Asc on 1/8/19.
+//  Copyright © 2019 Viet Asc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
